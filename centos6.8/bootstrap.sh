c77f48bd360450567aefdab4c1bce23e94829076
lhq <lhq@python279.org>
2017-01-06 22:10:51 +0800

    fix the bootstrap.sh

--- centos6.8/bootstrap.sh
+++ centos6.8/bootstrap.sh
@@ -2,7 +2,7 @@
 
 cp /vagrant/hosts /etc/hosts
 cp /vagrant/resolv.conf /etc/resolv.conf
-yum install ntp vim dos2unix -y
+yum install ntp -y
 service ntpd start
 service iptables stop
 mkdir -p /root/.ssh; chmod 600 /root/.ssh; cp /home/vagrant/.ssh/authorized_keys /root/.ssh/
