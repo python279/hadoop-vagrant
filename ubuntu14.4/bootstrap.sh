5010dc8e53aaa433d20051effeadc0fc64b8ec66
lhq <lhq@python279.org>
2017-02-18 21:50:34 +0800

    make ubuntu14.04 local

--- ubuntu14.4/bootstrap.sh
+++ ubuntu14.4/bootstrap.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 cp /vagrant/hosts /etc/hosts
 cp /vagrant/resolv.conf /etc/resolv.conf
-cp /vagrant/sources.list /etc/apt/sources.list
 mkdir -p /root/.ssh; chmod 600 /root/.ssh; cp /home/vagrant/.ssh/authorized_keys /root/.ssh/
+
+apt-get install -y openntpd
 
 cp /vagrant/insecure_private_key /root/insecure_private_key
 chmod 600 /root/insecure_private_key
