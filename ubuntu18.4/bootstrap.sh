14e4fc5c2a73fd4b4d3bef475283fb5c2b51b7ba
lhq <lhq@python279.org>
2021-02-21 17:45:24 +0800

    change language and tzdata

--- ubuntu18.4/bootstrap.sh
+++ ubuntu18.4/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
+
 cp /vagrant/hosts /etc/hosts
-cp /vagrant/resolv.conf /etc/resolv.conf
 
 mkdir -p /root/.ssh
 chmod 700 /root/.ssh
@@ -8,3 +8,10 @@ cp /home/vagrant/.ssh/authorized_keys /root/.ssh/
 
 cp /vagrant/insecure_private_key /root/insecure_private_key
 chmod 600 /root/insecure_private_key
+
+apt-get install -y locales openntpd tzdata
+locale-gen en_US.UTF-8
+dpkg-reconfigure -f noninteractive locales
+rm -f /etc/localtime
+ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+dpkg-reconfigure -f noninteractive tzdata
